4e5ce0664eed0440c5a459b2a855e0f3ae1d9615
Mrjing <837317210@qq.com>
2020-10-19 10:20:10 +0800

    fix_bug

--- src/utils/cloudbase-request.ts
+++ src/utils/cloudbase-request.ts
@@ -1,6 +1,8 @@
 import { getAuth } from './auth'
 import { fetch } from './http-request'
 
+const SUPPORT_REGIONS = ['ap-shanghai', 'ap-guangzhou']
+
 export async function cloudBaseRequest(options: {
     config: {
         envId: string
@@ -23,14 +25,30 @@ export async function cloudBaseRequest(options: {
     const isInContainer = !!process.env.KUBERNETES_SERVICE_HOST // 是否容器环境
     // region 优先级 本地mock 注入 > init region > 云函数环境变量region
     const finalRegion = process.env.TCB_REGION || region || process.env.TENCENTCLOUD_REGION || ''
+    let internetRegionEndpoint = ''
+    let internalRegionEndpoint = ''
+    if (finalRegion) {
+        if (SUPPORT_REGIONS.includes(finalRegion)) {
+            internetRegionEndpoint = `${finalRegion}.tcb-api.tencentcloudapi.com`
+            internalRegionEndpoint = `internal.${finalRegion}.tcb-api.tencentcloudapi.com`
+        } else {
+            console.warn('当前仅支持上海，广州地域，其他地域默认解析到固定域名(上海地域)')
+            internetRegionEndpoint = `tcb-api.tencentcloudapi.com`
+            internalRegionEndpoint = `internal.tcb-api.tencentcloudapi.com`
+        }
+    } else {
+        internetRegionEndpoint = `tcb-api.tencentcloudapi.com`
+        internalRegionEndpoint = `internal.tcb-api.tencentcloudapi.com`
+    }
 
     // 有地域信息则访问地域级别域名，无地域信息则访问默认域名，默认域名固定解析到上海地域保持兼容
-    const internetRegionEndpoint = finalRegion
-        ? `${region}.tcb-api.tencentcloudapi.com`
-        : `tcb-api.tencentcloudapi.com`
-    const internalRegionEndpoint = finalRegion
-        ? `internal.${region}.tcb-api.tencentcloudapi.com`
-        : `internal.tcb-api.tencentcloudapi.com`
+    // const internetRegionEndpoint = finalRegion
+    //     ? `${finalRegion}.tcb-api.tencentcloudapi.com`
+    //     : `tcb-api.tencentcloudapi.com`
+
+    // const internalRegionEndpoint = finalRegion
+    //     ? `internal.${finalRegion}.tcb-api.tencentcloudapi.com`
+    //     : `internal.tcb-api.tencentcloudapi.com`
 
     // 同地域走内网，跨地域走公网
     const isSameRegionVisit = region ? region === process.env.TENCENTCLOUD_REGION : true
